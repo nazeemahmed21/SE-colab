94492dcfb25f8a689a6b2d91dbe14b1562d7d9f5
Nazeem2023 <nazeem.intern@hyke.ai>
2024-01-28 00:53:50 +0400

    edited

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -3,7 +3,7 @@ import computer from "../images/giphy (1).gif"
 import logo from "../images/logo.png"
 import video from "../images/video-call.jpg"
 import wb from "../images/wb.jpg"
-import teams from "../images/tem.jpg"
+import teams from "../images/tems.jpg"
 import '../styles/landingpage.css'
 import { useNavigate } from 'react-router-dom'
 
@@ -28,7 +28,7 @@ const Landingpage = () => {
         <div className='container'>
           <div className='header'>Co-lab</div>
           <p className='slogan'>Learn to Connect. Connect to Colab</p>
-          <p className='desc'>insert description here</p>
+          <p className='desc'>Bridging innovations, unleashing possibilities</p>
         </div>
         <div>
           <button className='login' onClick={() =>navigate('/login')}>Login</button>
