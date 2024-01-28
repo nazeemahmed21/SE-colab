2c8ed4365d16339c46821fc105895c8dc24d14af
Shehryar Naeem <sn2054@hw.ac.uk>
2024-01-28 17:40:08 +0400

    added todo to the homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { PageTitle, AppHeader, AppContent } from "../components/combinedTodo";
+import styles from "../styles/todo.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
-  )
-}
+    <>
+      <div>
+        <div>
+          <Navbar />
+        </div>
+        <div className={styles.half}>
+          <div className="container">
+            <PageTitle>TO DO List</PageTitle>
+            <div className={styles.app__wrapper}>
+              <AppHeader />
+              <AppContent />
+            </div>
+          </div>
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                fontSize: "1.75rem",
+              },
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home;    
+export default Home;
