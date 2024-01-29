6d66561132c2d754651c58695a4c98fb04c937ce
fathima106 <fz@developl-collab.com>
2024-01-29 12:06:51 +0400

    labs almost done

--- src/pages/labDetails.js
+++ src/pages/labDetails.js
@@ -1,50 +1,53 @@
 // LabDetails.js
 
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
 import '../styles/labs.css';
+import folder from "../images/folder.png"
+import ppt from "../images/ppt.png"
 
 const LabDetails = () => {
- 
   return (
     <div>
       <Navbar />
 
-      <div className='LabsPage'>
-      <Link to="/labs">
-         <span className='back-arrow'>← Back</span>
-        </Link>
-        <h1>Lab A</h1>
-
-            {/* Sidebar with lab members */}
-            <div className="labSideBar">
-              <div className="labSideBarText">
-              <ul>
-                <p>Lab Files</p>
-                <p>Lab Members</p>
-                <Link to="/labAnnouncements">
-                <p>Announcements</p>
-                </Link>
-                <p>Leave Lab</p>
-              </ul>
-              </div>
-            </div>
-      
-
-        <div className='LabsButtons'>
-          <button className='LabsButton'>Add a file</button>
-          <button className='LabsButton'>Add a folder</button>
+      <div className='LabsPageAnnouncement'>
+        {/* Sidebar with lab members */}
+        <div className="labSideBar">
+          <div className="labSideBarText">
+            <ul>
+              
+              <p>Lab Files</p>
+              <p>Lab Members</p>
+              <Link to="/labAnnouncements">
+              <p>Announcements</p>
+              </Link>
+              <p>Leave Lab</p>
+            </ul>
+          </div>
         </div>
 
-        <div className='LabsContainer'>
-
-            {/* Link to navigate to Lab Details */}
-            <Link to="/labDetails">
-              <div className="lab" style={{ backgroundImage: 'url("lab_a_bg.jpg")' }}>
-                <p>Folder 1</p>
-              </div>
-            </Link>
+        <div className="LabsContent">
+          <Link to="/labs">
+            <span className='back-arrow'>←</span>
+          </Link>
+          <h1>Lab A</h1>
+          <div className='LabsButtons'>
+        <button className='LabsButton'>Add a file</button>
+        <button className='LabsButton'>Add a folder</button>
+        </div>
+        <div className='labFolderContainer'>
+          <div className="imageContainer">
+            <img src={folder} alt="folder" />
+            <p>Folder</p>
+          </div>
+          <div className="imageContainer">
+            <img src={ppt} alt="ppt" />
+            <p>PPT</p>
+          </div>
+        </div>
+        
         </div>
       </div>
     </div>
--- src/pages/labsAnnouncement.js
+++ src/pages/labsAnnouncement.js
@@ -1,47 +1,48 @@
 // LabDetails.js
 
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
 import '../styles/labs.css';
 
-const LabAnnouncements= () => {
- 
+const LabAnnouncements = () => {
   return (
     <div>
       <Navbar />
 
       <div className='LabsPageAnnouncement'>
-      <Link to="/labs">
-          <span className='back-arrow'>← Back</span>
-        </Link>
-        <h1>Lab A</h1>
-        <h1>Announcements</h1>
-
-            {/* Sidebar with lab members */}
-            <div className="labSideBar">
-              <div className="labSideBarText">
-              <ul>
-                <p>Lab Files</p>
-                <p>Lab Members</p>
-                <p>Announcements</p>
-                <p>Leave Lab</p>
-              </ul>
-              </div>
-            </div>
-      
-
-        <div className='LabsButtons'>
-          <button className='LabsButton'>Add a file</button>
-          <button className='LabsButton'>Add a folder</button>
+        {/* Sidebar with lab members */}
+        <div className="labSideBar">
+          <div className="labSideBarText">
+            <ul>
+            <Link to="/labDetails">
+              <p>Lab Files</p>
+              </Link>
+              <p>Lab Members</p>
+              <p>Announcements</p>
+              <p>Leave Lab</p>
+            </ul>
+          </div>
         </div>
 
-        <div className='LabsContainer'>
-            <Link to="/labDetails">
-              <div className="lab" style={{ backgroundImage: 'url("lab_a_bg.jpg")' }}>
-                <p>Folder 1</p>
-              </div>
-            </Link>
+        <div className="LabsContent">
+          <Link to="/labs">
+            <span className='back-arrow'>←</span>
+          </Link>
+          <h1>Lab A</h1>
+       
+          <h1>Announcements</h1>
+
+          <div className='LabsAnContainer'>
+            <div className="labAnncMessage" style={{ backgroundImage: 'url("lab_a_bg.jpg")' }}>
+              <h1>Admin: Hey guys, please get your work done before the deadline.</h1>
+              <p>Sent at 6:30 on January 25, 2024</p>
+            </div>
+            <div className="labAnncMessage" style={{ backgroundImage: 'url("lab_a_bg.jpg")' }}>
+              <h1>Admin: Remember to message me if there are any doubts</h1>
+              <p>Sent at 6:30 on January 25, 2024</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
