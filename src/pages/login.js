73e5bb279d4f6a5a5063c6a699d675e927d5e4d3
Nazeem2023 <nazeem.intern@hyke.ai>
2024-01-30 23:21:49 +0400

    email-link-verified

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,20 +11,43 @@ function Login() {
   const passwordRef = useRef();
   const navigate = useNavigate(); // Initialize useNavigate
 
-  const handleLogin = async () => {
+  // const handleLogin = async () => {
+  //   const email = emailRef.current.value;
+  //   const password = passwordRef.current.value;
+
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     // Redirect to the homepage upon successful login
+  //     navigate('/home');
+  //   } catch (error) {
+  //     console.error('Login failed:', error);
+  //     // Handle login failure here (e.g., show an error message)
+  //   }
+  // };
+  const handleLogin = (e) => {
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      // Redirect to the homepage upon successful login
-      navigate('/home');
-    } catch (error) {
-      console.error('Login failed:', error);
-      // Handle login failure here (e.g., show an error message)
-    }
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("hi");
+        // Check if the user exists and their email is verified
+        if (user && user.emailVerified) {
+          navigate("/home");
+        } else if (user && !user.emailVerified) {
+          // setFail(true);
+          // setErrMsg("Please verify your email before logging in.")
+          console.log("Please verify your email before logging in.");
+        }
+      })
+      .catch((error) => {
+        const errorMessage = `Error creating user: ${error}`;
+        // setErrMsg(errorMessage);
+        // setFail(true);
+        console.log(errorMessage);
+      });
   };
-
   return (
     <div className='login-container'>
       <div className='l-form-container'>
