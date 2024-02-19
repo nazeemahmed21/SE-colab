bc1aefa05e27044e3e43774ad86dcdafb0bf8dd0
fathima106 <fz@developl-collab.com>
2024-02-20 00:22:53 +0400

    update

--- src/components/ForwardDialog.js
+++ src/components/ForwardDialog.js
@@ -3,21 +3,28 @@ import React from 'react';
 import '../Style.css';
 
 const ForwardDialog = ({ messageId, messageText, users, onClose, onForward }) => {
-  return (
+    console.log(users);
+
+    return (
     <div className="forward-dialog">
       <h2>Forward Message</h2>
       <p>{messageText}</p>
       <div className="button-container">
-        {users && users.length > 0 ? (
+        {users && users.length > 0 ? (  
           users.map((user) => (
-            <button key={user.id} onClick={() => onForward(messageId, user.id)}>
-              {user.name}
+            <button key={user
+            .id} onClick={() => onForward(messageId, user.id)}>
+              {/* {user.name} */}
+              {users}
             </button>
           ))
         ) : (
           <p>No users available</p>
         )}
+        
          <button onClick={onClose}>Forward</button>
+         
+
         <button onClick={onClose}>Cancel</button>
       </div>
     </div>
