18df45817cc8971db12da5d4c82285b08bdd5d40
fathima106 <fz@developl-collab.com>
2024-03-19 17:53:47 +0400

    audio done

--- src/pages/meditationPlayer.js
+++ src/pages/meditationPlayer.js
@@ -1,14 +1,25 @@
 // MeditationPlayer.js
-import React from 'react';
+import React,{ useState, useRef, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-// import meditation1audio1 from './components/Meditation1/IntroductionToMeditation.m4a';
+import meditationAudio1 from '../components/Meditation1/IntroductionToMeditation.m4a';
+import meditationAudio2 from '../components/Meditation1/BreathingExercise.m4a';
+import meditationPic1 from '../components/Meditation1/IntroductionToMeditation.jpg';
+import  meditationPic2 from '../components/Meditation1/BreathingExercise.jpg';
+import '../styles/meditation1.css';
+import Navbar from '../components/Navbar';
+import pause from '../images/icons8-pause-button-50.png';
+import play from '../images/icons8-circled-play-50.png';
+import soundwave from '../images/sound-wave.gif';
 
-// Define your data mapping titles to corresponding picture, audio, and quotes
+// https://icons8.com/icons/set/sound-wave--animated 
+// Circled Play  and pause buttons from Icons8.com https://icons8.com/icons/set/play-button
+
+
 const meditationData = {
     "Introduction to Meditation": {
-      picture: "path/to/introduction_picture.jpg",
+      picture: meditationPic1,
     //   https://marshalucasphd.com/basic-mindfulness-practice-audio-download-marsha-lucas-phd/
-      audio: "meditation1audio1",
+      audio: meditationAudio1,
       quotes: [
         "The present moment is the only moment available to us, and it is the door to all moments. - Thich Nhat Hanh",
         "In today's rush, we all think too much, seek too much, want too much, and forget about the joy of just being. - Eckhart Tolle",
@@ -16,9 +27,10 @@ const meditationData = {
       ]
     },
     "Breathing Techniques": {
-        picture: "path/to/introduction_picture.jpg",
+        // https://www.nivati.com/blog/what-is-4-7-8-breathing
+        picture: meditationPic2,
         // https://www.youtube.com/watch?v=enJyOTvEn4M
-        audio: "https://www.youtube.com/watch?v=enJyOTvEn4M",
+        audio: meditationAudio2,
         quotes: [
           "Feelings come and go like clouds in a windy sky. Conscious breathing is my anchor. - Thich Nhat Hanh",
           "The breath is the king of the mind. - B.K.S. Iyengar",
@@ -41,27 +53,53 @@ const meditationData = {
   const MeditationPlayer = ({ match }) => {
     // Extract parameters from the route
     const { title } = useParams();
-  
-    // Get data for the current title
     const { picture, audio, quotes } = meditationData[title];
-  
-    return (
-      <div>
-        <h1>Meditation Player</h1>
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+   
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          if (currentQuoteIndex < quotes.length - 1) {
+            setCurrentQuoteIndex(currentQuoteIndex + 1);
+          }
+        }, 30000); // 30 seconds interval
+    
+        return () => clearInterval(intervalId);
+      }, [currentQuoteIndex, quotes.length]);
+    const toggleAudio = () => {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    };
+
+    return ( <div className='meditationPlayerPage' style={{ backgroundImage: `url(${picture})`, backgroundSize: 'cover' }}>
+{/* <Navbar /> */}
+
+        <div className='meditationPlayerTitle'>
         <h2>{title}</h2>
-        <img src={picture} alt="Meditation" />
-        <audio controls>
-          <source src={audio} type="audio/mp3" />
-          Your browser does not support the audio element.
-        </audio>
-        <div>
-          <h3>Quotes for {title}</h3>
-          <ul>
-            {quotes.map((quote, index) => (
-              <li key={index}>{quote}</li>
-            ))}
-          </ul>
         </div>
+        <br />
+        <div className='meditationPlayerQuote'>
+          <ul>
+          {quotes[currentQuoteIndex]}
+        </ul>
+        </div>
+        <audio ref={audioRef} src={audio} type="audio/mp3" controls />
+        {isPlaying && <img src={soundwave} alt="Sound Wave" style={{ height: '120px' }} />}
+        <div className="buttonContainer">
+  <button className="playPauseButton" onClick={toggleAudio}>
+    {isPlaying ? <img src={pause} alt="Pause" style={{ height: '120px' }} /> : <img src={play} alt="Play" style={{ height: '120px' }} />}
+  </button>
+</div>
+
+
+ <br/>
+      {isPlaying && <div className="musicWave"></div>}
+     
       </div>
     );
   };
