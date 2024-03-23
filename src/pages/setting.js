794dd925e8ef340398ec4141807d35838675ca12
an2048 <an2048@hw.ac.uk>
2024-03-23 11:44:09 +0400

    button for account deletion

--- src/pages/setting.js
+++ src/pages/setting.js
@@ -1,26 +1,28 @@
-  import React, { useState } from "react";
-  import Navbar from "../components/Navbar";
-  import "../styles/settings.css";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import "../styles/settings.css";
+import { deleteSignedUser } from "../firebase";
 
-  const Settings = () => {
-    const [darkMode, setDarkMode] = useState(false);
+const Settings = () => {
+  const [darkMode, setDarkMode] = useState(false);
 
-    const toggleDarkMode = () => {
-      setDarkMode(!darkMode);
-    };
-
-    return (
-      <div className={darkMode ? "dark-mode" : ""}>
-        <Navbar />
-        <div>
-          <p className="settings_header">Settings</p>
-          <div className="settings_theme">
-            <p >Theme:</p>
-            <button className="settings_theme_btn" onClick={toggleDarkMode}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
-          </div>
-        </div>
-      </div>
-    );
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
   };
 
-  export default Settings;
+  return (
+    <div className={darkMode ? "dark-mode" : ""}>
+      <Navbar />
+      <div>
+        <p className="settings_header">Settings</p>
+        <div className="settings_theme">
+          <p >Theme:</p>
+          <button className="settings_theme_btn" onClick={toggleDarkMode}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
+          <button className="delete_acc_btn" onClick={deleteSignedUser}>Delete Account</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
